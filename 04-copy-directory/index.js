2baf290f2f2ed6b01ec0a9038a368efaea7ee07f
SashaTsarikova <Sashatsarikova@mail.ru>
2021-11-07 23:40:01 +0300

    feat: add 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
 
+const path = require('path');
+const fs = require('fs');
+
+fs.mkdir(path.join(__dirname, 'files-copy'),{ recursive: true }, err => {
+    if (err) throw err;
+});
+
+
+fs.promises.readdir(path.join(__dirname,"files"))
+    .then(files => {
+for (let filename of files) {
+    function callback(err) {
+        if (err) throw err;
+      }
+      // destination.txt will be created or overwritten by default.
+      fs.copyFile(path.join(__dirname,'files',filename), path.join(__dirname,'files-copy',filename), callback);
+      }
+})
+    .catch(err => {
+        console.log(err)
+    })
